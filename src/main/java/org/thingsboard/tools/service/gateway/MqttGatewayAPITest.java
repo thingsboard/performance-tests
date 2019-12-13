0182507d7e28587c8fb5650ae76701fbc4835ed5
Volodymyr Babak <volodymyr.babak@gmail.com>
2019-12-13 19:37:44 +0200

    Fixed credentials update

--- src/main/java/org/thingsboard/tools/service/gateway/MqttGatewayAPITest.java
+++ src/main/java/org/thingsboard/tools/service/gateway/MqttGatewayAPITest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -43,6 +43,11 @@ public class MqttGatewayAPITest extends AbstractAPITest implements GatewayAPITes
     int gatewayEndIdx;
 
     private List<Device> gateways = Collections.synchronizedList(new ArrayList<>(1024));
+
+    @Override
+    public void createDevices() throws Exception {
+        createDevices(false);
+    }
 
     @Override
     public void createGateways() throws Exception {
